0baef73714aa06c7820baec81fc4c6faaee7eacd
mayaosbourne <mayajosbourne@gmail.com>
2014-04-18 15:50:30 -0700

    made changes for the tilemap stuff

--- Game.js
+++ Game.js
@@ -6,11 +6,27 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 function preload() {
 
 	game.load.image('banana', 'seductive.jpg');
+	
+	//Load the tilemap file
+    game.load.tilemap('myGame', 'level_1.json', null, Phaser.Tilemap.TILED_JSON);
+    
+	//Load the spritesheet for the tilemap
+    game.load.image('tiles', 'assets/main.png');
 
 }
 
 function create() {
 
+	map = game.add.tilemap('myGame');
+	
+	//'main' is the name of the spritesheet inside of Tiled Map Editor
+	
+    map.addTilesetImage('main', 'tiles');
+    
+	//'Grass 1' is the name of a layer inside of Tiled Map Editor
+    layer = map.createLayer('Grass 1');
+    layer.resizeWorld();
+    
 	banana = game.add.sprite(100, 100, 'banana'); 
 	
 }

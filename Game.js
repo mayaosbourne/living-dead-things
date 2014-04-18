265052f910383d620722f70c84d257fa8ffeda87
mayaosbourne <mayajosbourne@gmail.com>
2014-04-18 16:48:31 -0700

    fixed bug in game.js

--- Game.js
+++ Game.js
@@ -11,18 +11,18 @@ function preload() {
     game.load.tilemap('myGame', 'level_1.json', null, Phaser.Tilemap.TILED_JSON);
     
 	//Load the spritesheet for the tilemap
-    game.load.image('tiles1', 'assets/tilesets/down_cave.png');
-    game.load.image('tiles2', 'assets/tilesets/right_cave.png');
-    game.load.image('tiles3', 'assets/tilesets/up_cave.png');
-    game.load.image('tiles4', 'assets/tilesets/stars.png');
-    game.load.image('tiles5', 'assets/tilesets/tile_01.png');
-    game.load.image('tiles6', 'assets/tilesets/tile_02.png');
-    game.load.image('tiles7', 'assets/tilesets/tile_03.png');
-    game.load.image('tiles8', 'assets/tilesets/tile_04.png');
-    game.load.image('tiles10', 'assets/tilesets/tile_05.png');
-    game.load.image('tiles11', 'assets/tilesets/tile_06.png');
-    game.load.image('tiles12', 'assets/tilesets/tile_07.png');
-    game.load.image('tiles13', 'assets/tilesets/tile_08.png');
+    game.load.image('tiles1', 'assets/\tilesets/\down_cave.png');
+    game.load.image('tiles2', 'assets/\tilesets/\right_cave.png');
+    game.load.image('tiles3', 'assets/\tilesets/\up_cave.png');
+    game.load.image('tiles4', 'assets/\tilesets/\stars.png');
+    game.load.image('tiles5', 'assets/\tilesets/tile_01.png');
+    game.load.image('tiles6', 'assets/\tilesets/\tile_02.png');
+    game.load.image('tiles7', 'assets/\tilesets/\tile_03.png');
+    game.load.image('tiles8', 'assets/\tilesets/\tile_04.png');
+    game.load.image('tiles9', 'assets/\tilesets/\tile_05.png');
+    game.load.image('tiles10', 'assets/\tilesets/\tile_06.png');
+    game.load.image('tiles11', 'assets/\tilesets/\tile_07.png');
+    game.load.image('tiles12', 'assets/\tilesets/\tile_08.png');
     
 }
 
@@ -44,7 +44,6 @@ function create() {
     map.addTilesetImage('main9', 'tiles10');
     map.addTilesetImage('main10', 'tiles11');
     map.addTilesetImage('main11', 'tiles12');
-    map.addTilesetImage('main12', 'tiles13');
     
 	//'Grass 1' is the name of a layer inside of Tiled Map Editor
     layer = map.createLayer('Tile Layer 1');

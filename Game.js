ac378ca3c32fd9ffc74e542dda1b0e05386b4d10
mayaosbourne <mayajosbourne@gmail.com>
2014-04-21 00:17:41 -0700

    Got collision detection working for the ground and platforms.

--- Game.js
+++ Game.js
@@ -4,13 +4,15 @@
 var game = new Phaser.Game(800, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
-	game.load.atlasXML('player', 'assets/player/marco_sheet.png', 'assets/player/marco_sheet.xml')
-	game.load.image('ground', 'assets/seductive.jpg');
+	game.load.atlasXML('player', 'assets/player/marco_sheet.png', 'assets/player/marco_sheet.xml');
+	
+	//game.load.image('ground', 'assets/seductive.jpg');
+	
     //game.load.image('stand', 'assets/player/standing.png');
     //game.load.spritesheet('player', 'assets/player/marco_sheet.png', 47, 40);
 	
 	//Load the tilemap file
-    game.load.tilemap('myGame', 'test_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('map', 'test_map1.json', null, Phaser.Tilemap.TILED_JSON);
 	//Load the spritesheet for the tilemap
     game.load.image('tiles1', 'assets/tilesets/tile_04.png');
     
@@ -27,28 +29,36 @@ var downKey;
 var fireKey;
 
 function create() {
+	
+    
+    //  We're going to be using physics, so enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.P2JS);
+ 
+    
 	jumping = false;
     game.stage.backgroundColor = '#736357';
 
     //  In this example we'll create 4 specific keys (up, down, left, right) and monitor them in our update function
-    upKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
-    downKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    rightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+    leftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
     jumpKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     fireKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
-	map = game.add.tilemap('myGame');
+	map = game.add.tilemap('map');
 	
 	//'main' is the name of the spritesheet inside of Tiled Map Editor
 	
-    map.addTilesetImage('main', 'tiles1');
+    map.addTilesetImage('tiles1');
     
 	//'Grass 1' is the name of a layer inside of Tiled Map Editor
     layer = map.createLayer('Tile Layer 1');
     layer.resizeWorld();
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
- 
+    
+    map.setCollisionBetween(70, 77, true, 'Tile Layer 1');
+    map.setCollisionBetween(0, 20, true, 'Tile Layer 1');
+
+
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
  
@@ -56,13 +66,13 @@ function create() {
     platforms.enableBody = true;
  
     // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    //var ground = platforms.create(0, game.world.height - 64, layer);
  
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(3, 3);
+    //ground.scale.setTo(3, 3);
  
     //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
+    //ground.body.immovable = true;
     
     player = game.add.sprite(200, 360, 'player');
     player.animations.add('run left', [0, 1, 2, 3, 4, 5], 10, true);
@@ -70,7 +80,7 @@ function create() {
     player.animations.add('idle', [12, 13, 14, 15, 16, 17], true);
     
     player.animations.play('idle', 10);
-    
+
     game.physics.arcade.enable(player);
     
     //  Player physics properties. 
@@ -83,8 +93,8 @@ function create() {
 function update() {
 
 //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
-	if (jumpKey.isDown && player.body.touching.down)
+    game.physics.arcade.collide(player, layer);
+	if (jumpKey.isDown)
     {
     	jumping = true;
         player.body.velocity.y = -250;
@@ -94,18 +104,18 @@ function update() {
     }else{
     	player.body.velocity.x = velocity;
     }
-	if (leftKey.isDown && player.body.touching.down)
+	if (leftKey.isDown)
     {
-		player.body.velocity.x = -250;
+		player.x -= 2;
     	player.animations.play('run left', 10);
         velocity = player.body.velocity.x;
     }
-    else if (rightKey.isDown && player.body.touching.down)
+    else if (rightKey.isDown)
     {
-    	player.body.velocity.x = 250;
+    	player.x += 2;
     	player.animations.play('run right', 10);
     	velocity = player.body.velocity.x;
-    }else if (player.body.touching.down){
+    }else if (player.body.velocity.x === 0){
     	player.animations.play('idle', 10, true);
     	player.body.velocity.x = 0;
     	velocity = player.body.velocity.x;

1c10ade67357422d5d25ba1c9507087d0b792761
theomnix <ajmtallone@gmail.com>
2014-05-20 22:17:01 -0700

    Final commit for deadline.

--- Game.js
+++ Game.js
@@ -454,8 +454,8 @@ function handlePlayerMonsterCollision(){
 
 function initPlayer() {
 	
-    player = game.add.sprite(3000, 200, 'player');
-    //player = game.add.sprite(600, 600, 'player');
+    //player = game.add.sprite(3000, 200, 'player');
+    player = game.add.sprite(600, 800, 'player');
     player.animations.add('shooting', [0, 1, 2, 3], 5, true);
     player.animations.add('running', [4, 5, 6, 7, 8, 9], 10, true);
     player.animations.add('idle', [10, 11, 12, 13, 14, 15], 10, true);
